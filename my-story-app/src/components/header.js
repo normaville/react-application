a926a8ba1cbe23682c54f7a338385f2c8d1946dd
normaville <norma.ville.a@gmail.com>
2026-02-13 14:29:37 -0500

    added header

--- my-story-app/src/components/header.js
+++ my-story-app/src/components/header.js
@@ -1,44 +1,8 @@
-// Filename - Header.js
-
-import * as React from "react";
-
-// importing material UI components
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-
-export default function Header() {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                {/*Inside the IconButton, we 
-                    can render various icons*/}
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="menu"
-                    sx={{ mr: 2 }}
-                >
-                    {/*This is a simple Menu 
-                      Icon wrapped in Icon */}
-                    <MenuIcon />
-                </IconButton>
-                {/* The Typography component applies 
-                     default font weights and sizes */}
-
-                <Typography
-                    variant="h6"
-                    component="div"
-                    sx={{ flexGrow: 1 }}
-                >
-                    GeeksforGeeks Header
-                </Typography>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>
-    );
+function Header() {
+  return (
+    <header style={{ textAlign: 'center', padding: '20px', backgroundColor: '#f4f4f4' }}>
+      <h1>The Boy Who Cried Wolf</h1>
+    </header>
+  );
 }
+export default Header;
